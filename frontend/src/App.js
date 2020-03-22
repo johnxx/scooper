e7dde8cf7765f6a94042a41050dc92d660e7b866
John Bloom <john@sheeplauncher.net>
2020-03-22 03:11:27 -0700

    WIP: Reindent, start laying groundwork for new long-polling loop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,36 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import Gallery from 'react-photo-gallery';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {photos: []};
-  }
-  componentDidMount() {
-    const request = new Request('http://localhost:8000/api/media');
-    fetch(request)
-        .then(response => {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            throw new Error('Something went wrong on api server!');
-          }
-        })
-        .then(photos => {
-          this.setState({photos: photos});
-        });
-  }
+    constructor(props) {
+        super(props);
+        this.state = {photos: []};
+    }
+    componentDidMount() {
+        const request = new Request('http://localhost:8000/api/media/subreddit/1');
+        fetch(request)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong with the request!');
+                }
+            })
+            .then(photos => {
+                let newest =  photos.reduce(function(cur_newest, photo) {
+                    if(Date(photo.created_at) > cur_newest)
+                        return photo.created_at;
+                }, Date(1));
+                return this.setState({photos: photos});
+            });
+    }
 
-  render() {
-    return <div className="App">
-      <Gallery photos={this.state.photos}/>
-    </div>;
-  }
+    render() {
+        return <div className="App">
+            <Gallery photos={this.state.photos}/>
+        </div>;
+    }
 }
 
 export default App;
